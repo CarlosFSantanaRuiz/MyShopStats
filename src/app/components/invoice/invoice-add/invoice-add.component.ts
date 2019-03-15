e731730721fc681f16c859f7ca64e2f273e4995f
tylerjwhipple <tylerj.whipple@gmail.com>
2019-03-14 21:25:45 -0700

    Added Invoice Features

--- src/app/components/invoice/invoice-add/invoice-add.component.ts
+++ src/app/components/invoice/invoice-add/invoice-add.component.ts
@@ -7,6 +7,33 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class InvoiceAddComponent implements OnInit {
   @Output() notify: EventEmitter<string> = new EventEmitter;
+  shoplist = {
+    advisors: [
+      { firstName:"Bob", lastName:"Ford" },
+      { firstName:"Clarence", lastName:"Ford"  },
+      { firstName:"Tobias", lastName:"Ford"  }
+    ],
+    technicians: [
+      { firstName:"John", lastName:"Ford" },
+      { firstName:"Thomas", lastName:"Ford"  },
+      { firstName:"Elliott", lastName:"Ford"  }
+    ],
+    paymentOptions: [
+      { paymentOption:"Credit Card"},
+      { paymentOption:"Cash" },
+      { paymentOption:"Loan" }
+    ]
+   }
+  
+    invoice = {
+      isTaxExempt: false,
+      hasCheckSheet: false,
+      isComeBack: false,
+      labor: 0,
+      parts: 0,
+      total: 0
+    }
+  
   constructor() { }
 
   ngOnInit() {
@@ -14,6 +41,19 @@ export class InvoiceAddComponent implements OnInit {
 
   closeModal() {
     this.notify.emit('');
+  }
+
+  HandleCheckSheetException() {
+    this.invoice.hasCheckSheet = !this.invoice.hasCheckSheet;
+  }
+  HandleComebackException() {
+    this.invoice.isComeBack = !this.invoice.isComeBack;
+  }
+  HandleTaxExemptException() {
+    this.invoice.isTaxExempt = !this.invoice.isTaxExempt;
+  }
+  addTotal(value){
+    this.invoice.total = this.invoice.labor + this.invoice.parts;
   }
 
 }
--- src/app/components/invoice/invoice-list/invoice-list.component.ts
+++ src/app/components/invoice/invoice-list/invoice-list.component.ts
@@ -7,6 +7,27 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class InvoiceListComponent implements OnInit {
   @Output() notify: EventEmitter<string> = new EventEmitter;
+  
+  invoice = [{
+    invoiceNumber: 12385364,
+    technician: 'Tyler Whipple',
+    advisor: 'Carlos Santana Ruiz',
+    isTaxExempt: false,
+    hasCheckSheet: false,
+    isComeBack: false,
+    total: 23.50
+  },
+  {
+    invoiceNumber: 2566467,
+    technician: 'Bob Thomas',
+    advisor: 'John Smith',
+    isTaxExempt: true,
+    hasCheckSheet: true,
+    isComeBack: true,
+    total: 95.70
+  }
+]
+  
   constructor() { }
 
   ngOnInit() {
