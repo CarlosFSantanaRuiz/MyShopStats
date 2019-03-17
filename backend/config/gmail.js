d96e20a6edab93098ce137ebd1ee635c38997ba0
Carlos F. SantanaRuiz <48109252+CarlosFSantanaRuiz@users.noreply.github.com>
2019-03-16 20:30:11 -0600

    Delete gmail.js

--- backend/config/gmail.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    user: 'myshopstatistics@gmail.com',
-    clientId: '487888217087-0123jfcic6u4udf8ecrj364h8krf54il.apps.googleusercontent.com',
-    clientSecret: 'mcCtjwG1zL3zF6eDdVLLejCe',
-    refreshToken: '1/Be5lm7IvrjnGOOVzNOUNVi57emQnyIVN2kpDgarhEFo'
-};
